1e5a0f638e17e4d7055ccc955f51ecd8d9767161
Reza Rasuolzadeh <reza.rassoulzadeh@gmail.com>
2021-09-11 22:01:35 +0430

    complete splash

--- app/src/main/java/ir/rezarasuolzadeh/fruits/MainActivity.kt
+++ app/src/main/java/ir/rezarasuolzadeh/fruits/MainActivity.kt
@@ -6,19 +6,18 @@ import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import ir.rezarasuolzadeh.fruits.ui.nav.Navigation
-import ir.rezarasuolzadeh.fruits.ui.screens.LoginScreen
 import ir.rezarasuolzadeh.fruits.ui.theme.FruitsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_Fruits)
         setContent {
-            Navigation()
-//            FruitsTheme {
-//                Surface(color = MaterialTheme.colors.background) {
-//                    LoginScreen()
-//                }
-//            }
+            FruitsTheme {
+                Surface(color = MaterialTheme.colors.background) {
+                    Navigation()
+                }
+            }
         }
     }
 }
